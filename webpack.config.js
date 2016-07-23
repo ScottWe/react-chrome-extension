eddb5b80b681e56b9313d2bc2aeb020fb99a43ee
ScottWe <scott.wesley@ns.sympatico.ca>
2016-07-23 00:47:46 -0400

    Set up basic config to package.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'bin'),
+		filename: 'extension.bundle.js'
+	},
+	devtool: "source-map",
+	module: {
+		loaders: [{
+			test: /\.js(x)?/,
+			include: [
+				path.resolve(__dirname, 'src')
+			],
+			loader: "babel-loader"
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Popup",
+			filename: "popup.html"
+		}),
+		new CopyWebpackPlugin([
+				{
+					from: path.resolve(__dirname, "src/manifest.json"),
+					to: path.resolve(__dirname, "bin/manifest.json")
+				}
+			], {
+			copyUnmodified: true
+		})
+	]
+};
