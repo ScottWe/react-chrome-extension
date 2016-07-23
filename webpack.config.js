bf5c9b22079eda4dbb1ebc7df8febc3b5575b097
ScottWe <scott.wesley@ns.sympatico.ca>
2016-07-23 12:20:31 -0400

    Interpolating icons

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,35 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
+// Processes manifest
+const manifest = require('./src/manifest')
+
 // Variable presets
 const baseSrcPath = path.resolve(__dirname, 'src');
 const baseBinPath = path.resolve(__dirname, 'bin');
+
+// Interpolating dependencies from manifest
+var copyReqs = [
+	{
+		from: path.resolve(baseSrcPath, 'manifest.json'),
+		to: path.resolve(baseBinPath, 'manifest.json')
+	},
+	{
+		from: path.resolve(baseSrcPath, 'icon.png'),
+		to: path.resolve(baseBinPath, 'icon.png')
+	}
+];
+
+if (manifest.icons) {
+	for (var prop in manifest.icons) {
+		if (manifest.icons.hasOwnProperty(prop)) {
+			copyReqs.push({
+				from: path.resolve(baseSrcPath, prop),
+				to: path.resolve(baseBinPath, prop)
+			});
+		}
+	}
+}
 
 // Configurations
 module.exports = {
@@ -14,38 +40,29 @@ module.exports = {
 		path: baseBinPath,
 		filename: 'extension.bundle.js'
 	},
-	devtool: "source-map",
+	devtool: 'source-map',
 	module: {
 		loaders: [{
 			test: /\.js(x)?$/,
 			include: [
 				baseSrcPath
 			],
-			loader: "babel-loader"
+			loader: 'babel-loader'
 		}, {
 			test: /\.((scss)|(css))$/,
 			include: [
 				baseSrcPath
 			],
-			loaders: ["style", "css", "sass"]
+			loaders: ['style', 'css', 'sass']
 		}]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: "Popup",
-			filename: "popup.html",
-			template: path.resolve(baseSrcPath, "popup.html")
+			title: 'Popup',
+			filename: 'popup.html',
+			template: path.resolve(baseSrcPath, 'popup.html')
 		}),
-		new CopyWebpackPlugin([
-				{
-					from: path.resolve(baseSrcPath, "manifest.json"),
-					to: path.resolve(baseBinPath, "manifest.json")
-				},
-				{
-					from: path.resolve(baseSrcPath, "icon.png"),
-					to: path.resolve(baseBinPath, "icon.png")
-				}
-			], {
+		new CopyWebpackPlugin(copyReqs, {
 			copyUnmodified: true
 		})
 	]
